35578b240b3470f987f8acece7bd88aea527c3e2
Jonas Gao <gaopinsong@live.cn>
2023-08-24 13:33:24 +0800

    Custom window title bar

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@
     <el-container class='right-main-container'>
       <!-- tab container -->
       <el-main class='main-tabs-container'>
+        <TitleBar></TitleBar>
         <Tabs></Tabs>
       </el-main>
     </el-container>
@@ -30,6 +31,7 @@ import Aside from '@/Aside';
 import Tabs from '@/components/Tabs';
 import UpdateCheck from '@/components/UpdateCheck';
 import customCssLoader from './customCssLoader';
+import TitleBar from '@/components/TitleBar';
 
 const injectStylesheet = (src) => {
   customCssLoader.loadCustomCss(src);
@@ -61,14 +63,18 @@ export default {
     // restore side bar width
     this.restoreSideBarWidth();
   },
-  components: {Aside, Tabs, UpdateCheck},
+  components: {
+    TitleBar,
+    Aside,
+    Tabs,
+    UpdateCheck,
+  },
   methods: {
     bindSideBarDrag() {
       const that = this;
       const dragPointer = document.getElementById('drag-resize-pointer');
 
-      function mousemove(e)
-      {
+      function mousemove(e) {
         const mouseX = e.x;
         const dragSideWidth = mouseX - 17;
 
@@ -77,8 +83,7 @@ export default {
         }
       }
 
-      function mouseup(e)
-      {
+      function mouseup(e) {
         document.documentElement.removeEventListener('mousemove', mousemove);
         document.documentElement.removeEventListener('mouseup', mouseup);
 
@@ -94,13 +99,13 @@ export default {
       });
     },
     restoreSideBarWidth() {
-      let sideWidth = localStorage.sideWidth;
+      const { sideWidth } = localStorage;
       sideWidth && (this.sideWidth = sideWidth);
     },
     openHrefInBrowser() {
-      const shell = require('electron').shell;
+      const { shell } = require('electron');
 
-      document.addEventListener('click', function (event) {
+      document.addEventListener('click', (event) => {
         const ele = event.target;
 
         if (ele && (ele.nodeName.toLowerCase() === 'a') && ele.href.startsWith('http')) {
@@ -122,9 +127,9 @@ export default {
     },
     initZoom() {
       let zoomFactor = this.$storage.getSetting('zoomFactor');
-      zoomFactor = zoomFactor ? zoomFactor : 1.0;
+      zoomFactor = zoomFactor || 1.0;
 
-      const {webFrame} = require('electron');
+      const { webFrame } = require('electron');
       webFrame.setZoomFactor(zoomFactor);
     },
     loadCustomCss() {
@@ -150,6 +155,7 @@ export default {
 html {
   height: 100%;
 }
+
 body {
   height: 100%;
   padding: 8px;
@@ -164,10 +170,10 @@ body {
 button, input, textarea, .vjs__tree {
   font-family: inherit !important;
 }
+
 a {
   color: #8e8d8d;
 }
-
 
 /*fix el-select bottom scroll bar*/
 .el-scrollbar__wrap {
@@ -178,36 +184,45 @@ a {
 ::-webkit-scrollbar {
   width: 9px;
 }
+
 /*track*/
 ::-webkit-scrollbar-track {
   background: #eaeaea;
   border-radius: 4px;
 }
+
 .dark-mode ::-webkit-scrollbar-track {
   background: #425057;
 }
+
 /*track hover*/
 ::-webkit-scrollbar-track:hover {
   background: #e0e0dd;
 }
+
 .dark-mode ::-webkit-scrollbar-track:hover {
   background: #495961;
 }
+
 /*thumb*/
 ::-webkit-scrollbar-thumb {
   border-radius: 8px;
   background: #c1c1c1;
 }
+
 .dark-mode ::-webkit-scrollbar-thumb {
   background: #5a6f7a;
 }
+
 /*thumb hover*/
 ::-webkit-scrollbar-thumb:hover {
   background: #7f7f7f;
 }
+
 .dark-mode ::-webkit-scrollbar-thumb:hover {
   background: #6a838f;
 }
+
 /*scrollbar style end*/
 
 /*list index*/
@@ -218,6 +233,7 @@ li .list-index {
   margin-right: 10px;
   min-width: 28px;
 }
+
 .dark-mode li .list-index {
   color: #adacac;
 }
@@ -225,21 +241,25 @@ li .list-index {
 .wrap-container {
   height: 100%;
 }
+
 .aside-drag-container {
   position: relative;
   user-select: none;
   /*max-width: 50%;*/
 }
+
 .aside-connection {
   height: 100%;
   width: 100% !important;
   border-right: 1px solid #e4e0e0;
   overflow: hidden;
 }
+
 /*fix right container imdraggable*/
 .right-main-container {
   width: 10%;
 }
+
 .right-main-container .main-tabs-container {
   overflow-y: hidden;
   padding-top: 0px;
@@ -259,12 +279,14 @@ li .list-index {
   right: -12px;
   top: 0px;
 }
+
 #drag-resize-pointer {
   position: fixed;
   height: 100%;
   width: 10px;
   cursor: col-resize;
 }
+
 #drag-resize-pointer::after {
   content: "";
   display: inline-block;
@@ -279,12 +301,15 @@ li .list-index {
   bottom: 0;
   margin: auto;
 }
+
 .dark-mode #drag-resize-pointer::after {
   border-left: 1px solid #b9b8b8;
   border-right: 1px solid #b9b8b8;
 }
 
 @keyframes rotate {
-  to{ transform: rotate(360deg); }
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
